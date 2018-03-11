d30283cd3a25d38b3db7eff3d8047472fd12642a
Tony Su <peitaosu@163.com>
2018-03-11 23:03:36 +0800

    add reduce folder size implementation

---
+++ reduce_size.py
@@ -0,0 +1,30 @@
+import os, sys
+from utils import *
+
+UNI = {}
+DUP = {}
+
+def replace_file_with_symlink(src, dst):
+    remove_file(dst)
+    create_symlink(src, dst)
+
+def reduce_size(dir):
+    items = walk_folder(dir)
+    for item in items:
+        if os.path.isfile(item):
+            hash = md5_file(item)
+            if hash in UNI.keys():
+                replace_file_with_symlink(UNI[hash], item)
+                if hash in DUP.keys():
+                    DUP[hash].append(item)
+                else:
+                    DUP[hash] = [item]
+            else:
+                UNI[hash] = item
+    print 'DONE.'
+
+if __name__ == "__main__":
+    dir = '.'
+    if len(sys.argv) > 1:
+        dir = sys.argv[1]
+    reduce_size(dir)
