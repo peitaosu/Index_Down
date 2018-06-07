e18ff82fa78b34ee5219e4108c2554e873d5eda0
Tony Su <peitaosu@163.com>
2018-06-07 23:25:24 +0800

    support command line and rename some attributes

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import os, sys, json
+import os, sys, json, optparse
 from shutil import copyfile
 import urllib
 from utils import *
@@ -8,8 +8,8 @@ class Index():
     def __init__(self):
         self.dir_path = None
         self.index_file = None
-        self.remote = None
-        self.local = None
+        self.source = None
+        self.target = None
         self.UNI = {}
         self.DUP = {}
     
@@ -25,33 +25,33 @@ class Index():
     def get_index_file(self):
         return self.index_file
     
-    def set_remote(self, remote):
-        self.remote = remote
+    def set_source(self, source):
+        self.source = source
     
-    def get_remote(self):
-        return self.remote
+    def get_source(self):
+        return self.source
     
-    def set_local(self, local):
-        self.local = local
+    def set_target(self, target):
+        self.target = target
     
-    def get_local(self):
-        return self.local
+    def get_target(self):
+        return self.target
 
-    def _download(self, remote, local):
-        if remote.startswith(r"\\"):
+    def _download(self, source, target):
+        if source.startswith(r"\\"):
             # smb
-            copyfile(remote, local)
-        elif remote.startswith("http"):
+            copyfile(source, target)
+        elif source.startswith("http"):
             # http, https
-            urllib.urlretrieve (remote, local)
+            urllib.urlretrieve (source, target)
         else:
             # unknown
-            if not os.path.isdir(os.path.dirname(local)):
-                os.makedirs(os.path.dirname(local))
-            copyfile(remote, local)
+            if not os.path.isdir(os.path.dirname(target)):
+                os.makedirs(os.path.dirname(target))
+            copyfile(source, target)
 
-    def _duplicate(self, local, dup):
-        create_symlink(local, dup)
+    def _duplicate(self, target, dup):
+        create_symlink(target, dup)
 
     def _replace_file_with_symlink(self, src, dst):
         remove_file(dst)
@@ -73,12 +73,12 @@ class Index():
         with open(self.index_file) as in_file:
             index = json.load(in_file)
         for file_hash in index.keys():
-            remote_file = os.path.join(self.remote, index[file_hash][0])
-            local_file = os.path.join(self.local, index[file_hash][0])
-            self._download(remote_file, local_file)
+            source_file = os.path.join(self.source, index[file_hash][0])
+            target_file = os.path.join(self.target, index[file_hash][0])
+            self._download(source_file, target_file)
             if len(index[file_hash]) > 1:
                 for dup in index[file_hash][1:]:
-                    dup_file = os.path.join(self.local, dup)
+                    dup_file = os.path.join(self.target, dup)
                     self._duplicate(index[file_hash][0], dup_file)
 
     def reduce_size(self):
@@ -95,3 +95,44 @@ class Index():
                 else:
                     self.UNI[hash] = item
         print("DONE")
+
+def get_options():
+    parser = optparse.OptionParser()
+    parser.add_option("-p", "--path", dest="path",
+                      help="directory path")
+    parser.add_option("-i", "--index", dest="index",
+                      help="index file")
+    parser.add_option("-s", "--source", dest="source",
+                      help="source path")
+    parser.add_option("-t", "--target", dest="target",
+                      help="target path")
+    parser.add_option("-c", "--create", dest="create", action="store_true", default=False,
+                      help="create index")
+    parser.add_option("-d", "--download", dest="download", action="store_true", default=False,
+                      help="download from index")
+    parser.add_option("-r", "--reduce", dest="reduce", action="store_true", default=False,
+                      help="reduce size")
+    (options, args) = parser.parse_args()
+    if not (options.create or options.download or options.reduce):
+        parser.print_help()
+        sys.exit(-1)
+    return options
+
+if __name__ == "__main__":
+    index = Index()
+    opt = get_options()
+
+    if opt.path:
+        index.set_dir_path(opt.path)
+    if opt.index:
+        index.set_index_file(opt.index)
+    if opt.source:
+        index.set_source(opt.source)
+    if opt.target:
+        index.set_target(opt.target)
+    if opt.create:
+        index.create_index()
+    if opt.download:
+        index.download_from_index()
+    if opt.reduce:
+        index.reduce_size()
